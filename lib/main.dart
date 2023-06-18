51e489aec38d576684a4759cd80e07d2239d91aa
tynchtykestebes <tynchtyk.estebesuulu.bj@gmail.com>
2023-06-18 22:44:58 +0600

    just started

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Amazon Clone',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('Hello.'),
+        ),
+        body: const Center(
+          child: Text(
+            'Flutter Demo Home Page',
+          ),
+        ),
+      ),
+    );
+  }
+}
