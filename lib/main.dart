7fd166798f29263048aee7e0378961310e681b60
tynchtykestebes <tynchtyk.estebesuulu.bj@gmail.com>
2023-06-18 23:08:36 +0600

    just started

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:amazon_clone/constants/global_variables.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,17 +13,31 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Amazon Clone',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          iconTheme: IconThemeData(
+            color: Colors.black,
+          ),
+        ),
       ),
       home: Scaffold(
         appBar: AppBar(
           centerTitle: true,
           title: const Text('Hello.'),
         ),
-        body: const Center(
-          child: Text(
-            'Flutter Demo Home Page',
-          ),
+        body: Column(
+          children: [
+            const Center(
+              child: Text(
+                'Flutter Demo Home Page',
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Click'),
+            ),
+          ],
         ),
       ),
     );
