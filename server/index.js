1931c2a7efcb6fdd9ede1dc3839dada4951e6497
tynchtykestebes <tynchtyk.estebesuulu.bj@gmail.com>
2023-06-23 16:40:04 +0600

    sell product

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 // Imports from other files
 const authRouter = require("./routes/auth");
+const adminRouter = require("./routes/admin");
 
 // Init
 const PORT = 3000;
@@ -13,6 +14,7 @@ const DB =
 // middleware
 app.use(express.json());
 app.use(authRouter);
+app.use(adminRouter);
 
 // Connections
 mongoose
