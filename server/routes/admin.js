5cf942b52f7d7ca0a1b7d6db197eaa8d27ab2d0f
tynchtykestebes <tynchtyk.estebesuulu.bj@gmail.com>
2023-06-24 13:28:37 +0600

    fetching and displaying all products

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -21,4 +21,13 @@ adminRouter.post("/admin/add-product", admin, async (req, res) => {
   }
 });
 
+adminRouter.get("/admin/get-products", admin, async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.json(products);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
 module.exports = adminRouter;
